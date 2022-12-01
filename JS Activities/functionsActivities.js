783f4ffac47402df33f2a384ff72fde930dbcd05
Keligson <adamhealey@ymail.com>
2022-12-01 12:19:04 +0000

    Finished sorting functionsActivities.js, ready for OneFile

---
+++ JS Activities/functionsActivities.js
@@ -0,0 +1,18 @@
+// ACTIVITY 1
+// Create a function called sayHello that logs “Hi there, how are you?”
+
+// const sayHello = () => {
+//     console.log(`Hi there, how are you?`)
+// }
+
+// sayHello()
+
+
+// ACTIVITY 2
+// Add two parameters, myName and drink to your sayHello function. Log a sentence that says Hi and your name and offers a drink
+
+const sayHello = (myName, drink) => {
+    console.log(`Hi there, how are you? My name is ${myName}. Would you like a ${drink}`)
+}
+
+sayHello(`Adam`, `Irn Bru`)
