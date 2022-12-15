f45fbf4097016aece99f375ce5cd00c5ab1c1107
Keligson <adamhealey@ymail.com>
2022-12-15 15:57:32 +0000

    Completed functionsActivities.js

--- JS Activities/functionsActivities.js
+++ JS Activities/functionsActivities.js
@@ -1,5 +1,6 @@
-// ACTIVITY 1
+//--------- ACTIVITY 1 ---------
 // Create a function called sayHello that logs “Hi there, how are you?”
+
 
 // const sayHello = () => {
 //     console.log(`Hi there, how are you?`)
@@ -8,11 +9,17 @@
 // sayHello()
 
 
-// ACTIVITY 2
-// Add two parameters, myName and drink to your sayHello function. Log a sentence that says Hi and your name and offers a drink
 
-const sayHello = (myName, drink) => {
-    console.log(`Hi there, how are you? My name is ${myName}. Would you like a ${drink}`)
-}
 
-sayHello(`Adam`, `Irn Bru`)
+
+
+//--------- ACTIVITY 2 ---------
+// Add two parameters, myName and drink to your sayHello function
+// Log a sentence that says Hi and your name and offers a drink
+
+
+// const sayHello = (myName, drink) => {
+//     console.log(`Hi there, how are you? My name is ${myName}. Would you like an ${drink}?`)
+// }
+
+// sayHello(`Adam`, `Irn Bru`)
