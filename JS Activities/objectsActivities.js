1d2c176ae8e804f28536cab3de15cf4a95868dec
Keligson <adamhealey@ymail.com>
2022-12-16 17:11:21 +0000

    Finished functionsActivities.js. Updated objectsActivities.js

--- JS Activities/objectsActivities.js
+++ JS Activities/objectsActivities.js
@@ -1,31 +1,156 @@
-// ACTIVITY 1, 2, 3 and 4
-const pet = {
-    petName: "Bear",
-    typeOfPet: "dog",
-    age: 5,
-    colour: "Grey",
-    petFeatures: ["cute", "annoying", "cheese fiend"]
-}
+//--------- ACTIVITY 1 ---------
+// Create an object called pet with key values of petName, typeOfPet, age and colour
+// Use dot notation to log a sentence that describes your pet
 
-console.log(`I have a pet ${pet.typeOfPet} called ${pet.petName}. He's ${pet.age} years old and is ${pet.colour}. He's quite annoying, but too cute to be mad at.`)
+
+// let pet = {
+//     petName: "Bear",
+//     typeOfPet: "dog",
+//     age: 5,
+//     colour: "Grey",
+// }
+
+
+
+
+
+
+
+//--------- ACTIVITY 2 ---------
+// Change the colour of your pet
+
+
+// let pet = {
+//     petName: "Bear",
+//     typeOfPet: "dog",
+//     age: 5,
+//     colour: "Grey",
+// }
+
+// console.log(pet.colour)
+// pet.colour = "Brown"
+// console.log(pet.colour)
+
+
+
+
+
+
+
+
+//--------- ACTIVITY 3 ---------
+// Add a key value called petFeatures and add a list of features that describe your pet
+
+
+// const pet = {
+//     petName: "Bear",
+//     typeOfPet: "dog",
+//     age: 5,
+//     colour: "Grey",
+//     petFeatures: ["cute", "annoying", "cheese fiend"]
+// }
+
+// console.log(`I have a pet ${pet.typeOfPet} called ${pet.petName}. He's ${pet.age} years old and is ${pet.colour}. He's quite annoying, but too cute to be mad at.`)
+
+
+
+
+
+
+
+
+//--------- ACTIVITY 4 ---------
+// Add methods called eat and drink. They should return a string saying [Your pet name] is eating/drinking
+
+
+// const pet = {
+//     petName: "Bear",
+//     typeOfPet: "dog",
+//     age: 5,
+//     colour: "Grey",
+//     petFeatures: ["cute", "annoying", "cheese fiend"],
+//     eating: true,
+//     drinking: true,
+
+//     eatingOrDrinking () {
+//         if (this.eating == true && this.drinking == true) {
+//             return `${this.petName} is somehow both eating and drinking! Maybe they are having soup?`
+//         }
+//         else if (this.drinking == true) {
+//         return `${this.petName} is drinking.`
+//         }
+//         else if (this.eating == true) {
+//             return `${this.petName} is eating.`
+//         }
+//         else {
+//             return `${this.petName} is not drinking or eating at the moment. Maybe give them some cheese?`
+//         }
+//     }
+// }
+
+// console.log(pet.eatingOrDrinking())
+
+
+
+
+
+
+
+
 
 
 // ACTIVITY 5
+// Create an object called coffee shop with key values of branch, drinks with prices and food with prices
+// Add methods called drinksOrdered and foodOrdered
+// They should return a string saying [Your order] is … with all items chosen with costs, and the total cost
+
+
+// let totalCost
+
+// const coffeeShop = {
+//     branch: ["Oldham", "Manchester"],
+//     food: ["cereal", "chocolate bar", "apple"],
+//     drink: ["Irn Bru", "Pepsi", "Sprite"],
+//     price: [1, 1.50, 2, 2.50],
+//     foodOrder(f) {
+//         console.log(`You ordered a ${coffeeShop.food[f]}. The price is £${coffeeShop.price[f].toFixed(2)}.`)
+//         totalCost = coffeeShop.price[f]
+//     },
+//     drinkOrder(d) {
+//         totalCost = totalCost + coffeeShop.price[d]
+//         console.log(`You ordered a ${coffeeShop.drink[d]}. The price is £${coffeeShop.price[d].toFixed(2)}. Your total cost will be £${totalCost.toFixed(2)}`)
+//     }
+// }
+
+// coffeeShop.foodOrder(1), coffeeShop.drinkOrder(2)
+
+
 let totalCost
 
 const coffeeShop = {
     branch: ["Oldham", "Manchester"],
-    food: ["cereal", "chocolate bar", "apple"],
+    food: ["Cereal", "Chocolate bar", "Apple"],
     drink: ["Irn Bru", "Pepsi", "Sprite"],
     price: [1, 1.50, 2, 2.50],
-    foodOrder(f) {
-        console.log(`You ordered a ${coffeeShop.food[f]}. The price is £${coffeeShop.price[f].toFixed(2)}.`)
-        totalCost = coffeeShop.price[f]
-    },
-    drinkOrder(d) {
-        totalCost = totalCost + coffeeShop.price[d]
-        console.log(`You ordered a ${coffeeShop.drink[d]}. The price is £${coffeeShop.price[d].toFixed(2)}. Your total cost will be £${totalCost.toFixed(2)}`)
+
+    drinksOrdered (drinkNum) {
+    console.log(`You ordered: ${coffeeShop.drink[drinkNum]}. Price: ${coffeeShop.price[1]}`)
+    totalCost = ""
+    if (drinkNum == 2) {
+        coffeeShop.price[0]
+    }
+    else if (drinkNum == 3) {
+        coffeeShop.price[1]
+    }
+    // make if statement for Pepsi being 1 and Sprite being 1.50
+    }, 
+
+
+
+
+    foodOrdered (foodNum) {
+    console.log(`You ordered: ${coffeeShop.food[foodNum]}`)
     }
 }
 
-coffeeShop.foodOrder(1), coffeeShop.drinkOrder(2)
+coffeeShop.drinksOrdered(1)
--- JS Activities/oopsActivities.js
+++ JS Activities/oopsActivities.js
@@ -1,4 +1,9 @@
-// ACTIVITY 1
+//--------- ACTIVITY 1 ---------
+// Create a class called “Rental”
+// This class should have at least the name of the store, the store address, the store contact number, and a list of available movies for rental within the constructor
+// Create an object instance called “test” from your class and output it to the console to ensure it’s free from errors
+
+
 // class Rental {
 //     constructor (storeName, storeAddress, contactNum, availableMovies) {
 //     this.storeName = storeName;
@@ -9,11 +14,22 @@
     
 // }
 
-// const store1 = new Rental("Oldham", "2, Abbey road", "01611234567", "Shrek 1, Shrek 2, Shrek 3, Shrek 4: Unhinged")
-// console.log(store1)
+// const test = new Rental("Oldham", "2, Abbey road", "0161-123-4567", "Shrek, Shrek 2: Vengeance, Shrek 3: Back From the Dead, Shrek 4: Unhinged")
+// console.log(test)
 
 
-// ACTIVITY 2
+
+
+
+
+
+
+
+//--------- ACTIVITY 2 ---------
+// Write a function that can get all of the available movies and output them to the console
+// Write a function which can add a new movie to the list of available movies
+
+
 class Rental {
     constructor (storeName, storeAddress, contactNumber, availableMovies) {
     this.storeName = storeName;
@@ -64,3 +80,13 @@ console.log(store1.getAvailableMovies())
 
 // const dog6789 = new Pet("Bear", 5, "Dog", "Shih tzu", "Grey", "08:00");
 //    console.log(dog6789)
+
+
+
+
+
+
+
+//--------- ACTIVITY 3 ---------
+// Movies also have prices, refactor your code so that each movie also has an associated price attached
+// Output a list of all movies, including their prices, to the console when queried
